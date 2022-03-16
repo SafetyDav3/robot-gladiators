4b1133542c844d7e9f47e84a035d03e043cc01f9
SafteyDav3 <talktomedw@gmail.com>
2022-03-16 01:46:00 -0500

    Exploring var and alerts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
-window.alert("This is an alert! Javascript is running!");
+tslint: disabled;
+
+var playerName = window.prompt("What is the name of your robot?");
+
+console.log(playerName);
+var name = "your name";
+console.log(name);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// This creates a fight!
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+// fight();
