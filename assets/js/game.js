24d8a95558526d071bb4f8b849c5f0d8b0e216e2
SafteyDav3 <talktomedw@gmail.com>
2022-03-16 23:30:18 -0500

    About to start adding | if | else if | else

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,56 @@
-// JS Game
+// ↓↓↓ Robot Fighting Game ↓↓↓
 
-var playerName = window.prompt("What is the name of your Robot?");
+// →→→ Player Stats ←←←
+var playerName = window.prompt("What is the name of your robot?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+// →→→ Enemy Stats ←←←
 
-function fight() {
-  window.alert("The fight has begun!");
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// fight();
+// ↓↓↓ Fight function ↓↓↓
+
+var fight = function () {
+  // Alert players they are starting a round
+  window.alert("Welcome Robot Gladiators!");
+
+  // Subtract values "playerAttack" from "enemyHealth"
+  enemyHealth = enemyHealth - playerAttack;
+
+  // Log the results of player attack
+  console.log(
+    playerName +
+      " hit " +
+      enemyName +
+      " for " +
+      playerAttack +
+      " damage! " +
+      enemyName +
+      " only has " +
+      enemyHealth +
+      " health remaining!"
+  );
+  // Subtract values "enemyAttack" from "playerHealth"
+  playerHealth = playerHealth - enemyAttack;
+
+  // Log results of enemy attack
+  console.log(
+    enemyName +
+      " hit " +
+      playerName +
+      " for " +
+      enemyAttack +
+      " damage! " +
+      playerName +
+      " only has " +
+      playerHealth +
+      " health remaining!"
+  );
+};
+
+// ↓↓↓ Calling the fight function ↓↓↓
+
+fight();
