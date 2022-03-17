07c3b25ed0d4133fdc96fea6219c114f5b35dcf7
SafteyDav3 <talktomedw@gmail.com>
2022-03-16 22:59:32 -0500

    Lesson 3.1.6

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,11 @@
+// JS Game
+
+var playerName = window.prompt("What is the name of your Robot?");
+
+console.log(playerName);
+
 function fight() {
   window.alert("The fight has begun!");
 }
 
-fight();
+// fight();
